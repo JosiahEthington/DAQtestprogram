a4e9ce06454eb10a7f70acf004c56b1beb140076
Josiah Ethington <josiah.ethington@wsu.edu>
2018-11-26 12:48:58 -0800

    Initial Commit, program will create random data points and chart them live.  TODO: accept/decode data from UDP.

---
+++ DAQWinforms/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.WinForms;
+using LiveCharts.Configurations;
+
+
+namespace DAQWinforms
+{
+    public class MeasureModel
+    {
+        public System.DateTime DateTime { get; set; }
+        public double Value { get; set; }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var mapper = Mappers.Xy<MeasureModel>()
+    .X(model => model.DateTime.Ticks)   //use DateTime.Ticks as X
+    .Y(model => model.Value);           //use the value property as Y
+
+            //lets save the mapper globally.
+            Charting.For<MeasureModel>(mapper);
+
+            //the ChartValues property will store our values array
+            ChartValues = new ChartValues<MeasureModel>();
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = ChartValues,
+                    PointGeometrySize = 18,
+                    StrokeThickness = 4
+                }
+            };
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                DisableAnimations = true,
+                LabelFormatter = value => new System.DateTime((long)value).ToString("mm:ss"),
+                Separator = new Separator
+                {
+                    Step = TimeSpan.FromSeconds(1).Ticks
+                }
+            });
+
+            SetAxisLimits(System.DateTime.Now);
+
+            //The next code simulates data changes every 500 ms
+            Timer = new Timer
+            {
+                Interval = 500
+            };
+            Timer.Tick += TimerOnTick;
+            R = new Random();
+            Timer.Start();
+        }
+
+        public ChartValues<MeasureModel> ChartValues { get; set; }
+        public Timer Timer { get; set; }
+        public Random R { get; set; }
+
+        private void SetAxisLimits(System.DateTime now)
+        {
+            cartesianChart1.AxisX[0].MaxValue = now.Ticks + TimeSpan.FromSeconds(1).Ticks; // lets force the axis to be 100ms ahead
+            cartesianChart1.AxisX[0].MinValue = now.Ticks - TimeSpan.FromSeconds(8).Ticks; //we only care about the last 8 seconds
+        }
+
+        private void TimerOnTick(object sender, EventArgs eventArgs)
+        {
+            var now = System.DateTime.Now;
+
+            ChartValues.Add(new MeasureModel
+            {
+                DateTime = now,
+                Value = R.Next(0, 10)
+            });
+
+            SetAxisLimits(now);
+
+            //lets only use the last 30 values
+            if (ChartValues.Count > 30) ChartValues.RemoveAt(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartListener();
+        }
+        private const int listenPort = 11000;
+
+        private static void StartListener()
+        {
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    //TODO: Change this to reflect code recieved from UDP.
+                    Console.WriteLine($"Received broadcast from {groupEP} :");
+                    Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+    }
+
+}
