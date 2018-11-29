f26b9ac124d07d64d6699a7238aad425a9bd9ec1
Josiah Ethington <josiah.ethington@wsu.edu>
2018-11-29 03:11:34 -0800

    Attempting to get chart to update from broadcast.  Issues : axis labels could use work, only displays 1 sensor for temp.

--- DAQWinforms/Form1.cs
+++ DAQWinforms/Form1.cs
@@ -13,6 +13,8 @@ using LiveCharts;
 using LiveCharts.Wpf;
 using LiveCharts.WinForms;
 using LiveCharts.Configurations;
+using System.Web.Script.Serialization;
+//using System.Threading;
 
 
 namespace DAQWinforms
@@ -24,6 +26,7 @@ namespace DAQWinforms
     }
     public partial class Form1 : Form
     {
+        public SensorObj sensor1;
         public Form1()
         {
             InitializeComponent();
@@ -56,63 +59,51 @@ namespace DAQWinforms
             });
 
             SetAxisLimits(System.DateTime.Now);
-
-            //The next code simulates data changes every 500 ms
             Timer = new Timer
             {
-                Interval = 500
+                Interval = 1900
             };
+            //The next code simulates data changes every 500 ms
             Timer.Tick += TimerOnTick;
-            R = new Random();
             Timer.Start();
+            //Thread listenThread;
+            //listenThread = new Thread(new ThreadStart(StartListener));
+            //listenThread.Start();
         }
+        public System.Windows.Forms.Timer Timer { get; set; }
 
         public ChartValues<MeasureModel> ChartValues { get; set; }
-        public Timer Timer { get; set; }
-        public Random R { get; set; }
 
         private void SetAxisLimits(System.DateTime now)
         {
             cartesianChart1.AxisX[0].MaxValue = now.Ticks + TimeSpan.FromSeconds(1).Ticks; // lets force the axis to be 100ms ahead
-            cartesianChart1.AxisX[0].MinValue = now.Ticks - TimeSpan.FromSeconds(8).Ticks; //we only care about the last 8 seconds
+            cartesianChart1.AxisX[0].MinValue = now.Ticks - TimeSpan.FromSeconds(60).Ticks; //we only care about the last 8 seconds
         }
 
         private void TimerOnTick(object sender, EventArgs eventArgs)
         {
-            var now = System.DateTime.Now;
-
-            ChartValues.Add(new MeasureModel
-            {
-                DateTime = now,
-                Value = R.Next(0, 10)
-            });
-
-            SetAxisLimits(now);
-
-            //lets only use the last 30 values
-            if (ChartValues.Count > 30) ChartValues.RemoveAt(0);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            StartListener();
-        }
-        private const int listenPort = 11000;
-
-        private static void StartListener()
-        {
             UdpClient listener = new UdpClient(listenPort);
             IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
-
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            var now = System.DateTime.Now;
+            bool searching = true;
+            SensorObj temp = new SensorObj();
+            //double R = 0;
             try
             {
-                while (true)
+                while (searching)
                 {
-                    Console.WriteLine("Waiting for broadcast");
                     byte[] bytes = listener.Receive(ref groupEP);
-                    //TODO: Change this to reflect code recieved from UDP.
-                    Console.WriteLine($"Received broadcast from {groupEP} :");
-                    Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                    sensor1 = ser.Deserialize<SensorObj>(Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+                    if (sensor1.sensorID == "1" && sensor1.sensor == "temp")
+                    {
+                        ChartValues.Add(new MeasureModel
+                        {
+                            DateTime = now,
+                            Value = sensor1.value
+                        });
+                        searching = false;
+                    }
                 }
             }
             catch (SocketException e)
@@ -123,6 +114,88 @@ namespace DAQWinforms
             {
                 listener.Close();
             }
+
+
+            SetAxisLimits(now);
+
+            //lets only use the last 30 values
+            if (ChartValues.Count > 30) ChartValues.RemoveAt(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartListener();
+            //Sensor temp = new Sensor;
+            //temp.sensor = "something";
+            
+        }
+        private const int listenPort = 8888;
+
+        private void StartListener()
+        {
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    string temp = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+                    Console.WriteLine(temp);
+                    sensor1 = ser.Deserialize<SensorObj>(temp);
+                    //Console.WriteLine("Sensor:" + sensor1.ToString());
+
+                    //TODO: Change this to reflect code recieved from UDP.
+                    //Console.WriteLine($"Received broadcast from {groupEP} :");
+                    //Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+    }
+    public class SensorObj
+    {
+        public string sensor { get; set; }
+        public string sensorID { get; set; }
+        public double value { get; set; }
+        public override int GetHashCode()
+        {
+            return (sensor + sensorID).GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if(obj == null)
+            {
+                return false;
+            }
+            if(obj.GetType() == this.GetType())
+            {
+                Equals((SensorObj)obj);
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            return "Sensor: " + sensor + ";" + " SensorID: " + sensorID + ";" + " Value: " + value;
+        }
+
+        public bool Equals(SensorObj other)
+        {
+            if(this.sensor == other.sensor && this.sensorID == other.sensorID)
+            {
+                return true;
+            }
+            return false;
         }
     }
 
